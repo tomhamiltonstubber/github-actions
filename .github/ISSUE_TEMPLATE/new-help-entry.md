7a3d8a8825d9ac203ec7cf0cc3c56de74c81916f
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2019-11-28 18:00:46 +0000

    New page entry

--- .github/ISSUE_TEMPLATE/new-help-entry.md
+++ .github/ISSUE_TEMPLATE/new-help-entry.md
@@ -10,9 +10,9 @@ This is for a new help entry on an existing page.
 
 ## Glossary
 
-Page: The last part of the page's URL. For instance, if the URL is `/crm/help/administrator/what-is-an-administrator/`, use `what-is-an-administrator`.
-Title: The question or title of the help entry.
-Content: The answer to the question/help entry. This can include images.
+_Page_: The last part of the page's URL. For instance, if the URL is `/crm/help/administrator/what-is-an-administrator/`, use `what-is-an-administrator`.
+_Title_: The question or title of the help entry.
+_Content_: The answer to the question/help entry. This can include images.
 
 Page:
 Title:
--- create_help_article.py
+++ create_help_article.py
@@ -1,52 +1,67 @@
 import argparse
 import glob
 import os
+import random
 import re
+import string
 from base64 import b64encode
 
 import requests
-import uuid
 
 from github import Github, GithubException
 
-MD_IMAGE_REGEX = re.compile('(?:!\[.*?\]\((.*?)\))')
+MD_IMAGE_REGEX = re.compile('(?:(!\[.*?\])\((.*?)\))')
 BLOG_IMAGE_TMP = "{{ blog_image('%s') }}"
+NEW_FILE_TEMPLATE = """
+---
+title: {title}
+order: {order}
+alt: {alt_tags}
+related_posts:
+  - {related_1}
+  - {related_2}
+---
+{content}
+"""
 
 
 class HelpArticleCreator:
     def __init__(self, issue_number):
         github = Github(os.getenv('GITHUB_TOKEN'))
-        self.tc = github.get_repo('tomhamiltonstubber/github-actions')
-        self.issue = self.tc.get_issue(int(issue_number))
+        self.repo = github.get_repo('tomhamiltonstubber/github-actions')
+        self.issue = self.repo.get_issue(int(issue_number))
         self.branch_name = f'help-entry-{issue_number}'
 
     def run(self):
         self._get_create_branch()
-        if 'new-page' in self.issue.labels:
+        labels = [l.name for l in self.issue.labels]
+        if 'new-page' in labels:
             self.create_new_page()
-        elif 'documentation' in self.issue.labels:
+        elif 'documentation' in labels:
             self.create_new_entry()
         else:
             print('Not a help article.')
+            return
+        self._get_create_pr()
+
+    def _random_str(self):
+        return ''.join(random.choice(string.ascii_lowercase) for _ in range(6))
 
     def dl_images(self, page_name, body):
         image_urls = MD_IMAGE_REGEX.findall(body)
         img_paths = []
-        for url in image_urls:
-            img_path = f'/theme/assets/assets/help/{page_name}-{uuid.uuid4()}.{url.split(".", -1)}'
+        for i, (alt, url) in enumerate(image_urls):
+            img_path = f'theme/assets/assets/help/{page_name}-{i}.{url.split(".")[-1]}'
             r = requests.get(url, img_path)
             r.raise_for_status()
-            with open(img_path, 'w+') as f:
-                f.write(r.content)
             img_paths.append(img_path)
-
-            body.replace(url, BLOG_IMAGE_TMP % img_path.replace('/theme/assets', ''))
-            self._add_to_git(file_path=img_path, content=b64encode(r.content))
+            body = body.replace(f'{alt}({url})', BLOG_IMAGE_TMP % img_path.replace('/theme/assets', ''))
+            self._add_to_git(file_path=img_path, content=r.content)
         print(f'Downloaded {len(image_urls)} images')
         return body, img_paths
 
     def create_new_entry(self):
-        issue_content = self.issue.content
+        issue_content = self.issue.body
         try:
             page_name = re.search('Page:(.*)', issue_content).group(1).strip()
             title = re.search('Title:(.*)', issue_content).group(1).strip()
@@ -54,40 +69,91 @@ class HelpArticleCreator:
         except AttributeError:
             print('Error parsing template.')
             return
-        pages = glob.glob(f'/pages/help/**/{page_name}.md')
+        pages = glob.glob(f'pages/help/**/**/{page_name}.md', recursive=True)
         assert pages, f'Page "{page_name}" cannot be found.'
         page_path = pages[0]
 
         body, img_paths = self.dl_images(page_name, body)
-        with open(page_path, 'w+') as f:
-            f.write(f.read() + f'## {title}\n\n{body}')
-        fil_obj_sha = self.tc.get_contents(page_path, ref=self.branch_name).sha
-        self._add_to_git(file_path=page_path, content=body, sha=fil_obj_sha)
+
+        with open(page_path) as f:
+            old_content = f.read()
+        new_content = old_content + f'\n## {title}\n\n{body}'
+
+        self._add_to_git(file_path=page_path, content=new_content)
         return page_path, img_paths
 
+    def get_attr(self, term, body, dotall=False):
+        try:
+            return re.search(term, body, flags=[re.DOTALL] if dotall else 0).group(1).strip()
+        except AttributeError:
+            print(f"Couldn't find term '{term}' in body")
+
     def create_new_page(self):
-        pass
+        issue_content = self.issue.body
+        try:
+            page_name = self.get_attr('Page:(.*)', issue_content)
+            title = self.get_attr('Title:(.*)', issue_content)
+            body = self.get_attr('Content:(.*)', issue_content, re.DOTALL)
+            alt = self.get_attr('Alternative tags:(.*)', issue_content)
+            related_1 = self.get_attr('Related Post 1:(.*)', issue_content)
+            related_2 = self.get_attr('Related Post 2:(.*)', issue_content)
+        except AttributeError:
+            print('Error parsing template.')
+            return
+        pages = glob.glob(f'pages/help/**/**/{page_name}.md', recursive=True)
+        assert pages, f'Page "{page_name}" cannot be found.'
+        page_path = pages[0]
+
+        body, img_paths = self.dl_images(page_name, body)
+
+        new_content = NEW_FILE_TEMPLATE.format(
+            title=title,
+            alt=alt,
+            related_1=related_1,
+            related_2=related_2,
+            content=body
+        )
+
+        self._add_to_git(file_path=page_path, content=new_content)
+        return page_path, img_paths
 
     def _get_create_branch(self):
         # Creating new branch if needs be
         try:
-            self.tc.get_branch(self.branch_name)
+            self.repo.get_branch(self.branch_name)
         except GithubException as e:
             if e.args[1]['message'] == 'Branch not found':
-                self.tc.create_git_ref(f'refs/heads/{self.branch_name}', self.tc.get_commit('master').commit.sha)
+                master_commit = self.repo.get_commit('master').commit
+                self.repo.create_git_ref(f'refs/heads/{self.branch_name}', master_commit.sha)
             else:
                 raise
 
-    def _add_to_git(self, content, file_path, sha=None):
+    def _get_create_pr(self):
+        try:
+            self.repo.create_pull(title='Update Changelog', body='', base='master', head=self.branch_name)
+        except GithubException as e:
+            error = e.args[1]['errors'][0]
+            if 'A pull request already exists' not in error['message']:
+                raise
+        else:
+            print('Pull request created')
+
+    def _add_to_git(self, content, file_path):
+        file_path = file_path.lstrip('/')
         kwargs = dict(
             path=file_path,
-            message='Update Change Log',
+            message=f'Help entry for issue {self.issue.number}',
             content=content,
             branch=self.branch_name,
         )
-        if sha:
-            kwargs['sha'] = sha
-        self.tc.update_file(**kwargs)
+        try:
+            kwargs['sha'] = self.repo.get_contents(file_path, ref=self.branch_name).sha
+        except GithubException as e:  # File not in GIT yet.
+            if not e.args[1]['message'] == 'Not Found':
+                raise
+            self.repo.create_file(**kwargs)
+        else:
+            self.repo.update_file(**kwargs)
         print(f'Added {file_path} to Git')
 
 
